fc0a6ac28f8a7bfacf66c6421cbfbbe3fbdfbfab
Marina <marinapasnaeva@gmail.com>
2018-05-29 01:13:09 +0300

    homework

---
+++ Calc/Calc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Calc
+{
+    static class Calc
+    {
+
+         static Calc()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+
+        
+
+        public static int GetSub(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int GetSub(int a, int b, int c)
+        {
+            return a + b+c;
+        }
+
+
+        public static double GetSub(double a, double b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ Calc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 6, e = 6;
+            
+            double c = 10, d = 20;
+          
+
+            Console.WriteLine(Calc.GetSub(c, d));
+            Console.WriteLine(Calc.GetSub(a, b));
+            Console.WriteLine(Calc.GetSub(a, b, e));
+        }
+    }
+}
